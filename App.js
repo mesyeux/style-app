b5a4b1e0e38adece1140822135c6ca0730e67275
Chloe Lau <chlo@Chloes-MacBook-Pro.local>
2021-08-08 19:19:42 +0800

    Set up project

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!LALALALALLALALALALALAL</Text>
+      <form><button><p>hi</p></button></form>
       <StatusBar style="auto" />
     </View>
   );
